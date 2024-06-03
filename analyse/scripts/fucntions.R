1a7a0adb0f0ecace4a03ccc05f7135692b0c05f6
TheRedManPlays <gmodlovver@gmail.com>
2024-06-03 11:51:38 +0200

    finished working on my part of results, mm and README

--- analyse/scripts/fucntions.R
+++ analyse/scripts/fucntions.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(pwr)
 duplo_test <- function(get_oplossing, soort, data) {
     data %>%
         dplyr::filter(oplossing == get_oplossing & species == soort) %>%
@@ -13,7 +14,14 @@ cohens_d_duplo <- function(data, get_oplossing, soort, get_stats){
     n_1 <- ns[1]; n_2 <- ns[2]
     
     d <- get_stats * sqrt(1/n_1 + 1/n_2)
+}
 
+power_duplo <- function(data, get_oplossing, soort, get_stats){
+    new_data <- data %>%
+        dplyr::filter(oplossing == get_oplossing & species == soort)
+    n <- length(new_data)
+    t <- get_stats$statistic
+    d <- t / sqrt(n)
     
-        
+    pwr.t.test(n = n, d = d, sig.level = 0.05, type = "two.sample")
 }
